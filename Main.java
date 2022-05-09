5e70081bd9a237968d5ab9b53b301ba342bded66
Vu Nguyen <51254360+augcloud@users.noreply.github.com>
2022-05-08 21:05:58 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Controller ctrl = new Controller();
+        ctrl.displayMenu();
+        ctrl.readInput();
+    }
+}
